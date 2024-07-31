66e687303dacb1153b3583579944b20b201addaf
Adam <aedelen42@gmail.com>
2024-07-31 09:58:12 -0500

    initial version of custom lua vim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- vim config
+vim.cmd("set nu")
+vim.cmd("set rnu")
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Lazy Nvim
+require("config.lazy")
