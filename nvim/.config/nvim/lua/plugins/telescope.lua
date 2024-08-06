63712b400294ac1152bdacab51cbf96b8bdadacd
Adam <aedelen42@gmail.com>
2024-08-05 23:01:00 -0500

    adding telescope for git files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
 
       require("telescope").load_extension("ui-select")
     end,
