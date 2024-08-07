e896221a98bc09eff2215326a8163d6bd6d78cd6
Adam <aedelen42@gmail.com>
2024-08-06 21:27:21 -0500

    different blank line I guess. IDK but this was the suggested fix from code actions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,7 +20,7 @@ return {
 			lspconfig.lua_ls.setup({})
 			lspconfig.tsserver.setup({})
 			lspconfig.jdtls.setup({})
-			
+
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
