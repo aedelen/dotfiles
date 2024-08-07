cbe6a8e3d484d83b0c30387db2efe52bc061f466
Adam <aedelen42@gmail.com>
2024-08-06 21:24:13 -0500

    adding typescript and java lsp servers

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup {
-				ensure_installed = { "lua_ls" },
+				ensure_installed = { "lua_ls", "tsserver", "jdtls" },
 			}
 		end
 	},
@@ -18,6 +18,9 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.jdtls.setup({})
+			
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
