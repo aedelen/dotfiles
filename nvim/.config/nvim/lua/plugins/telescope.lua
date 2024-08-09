fdea825de3dbe7ecd9a51a456571100abac814f5
Adam <aedelen42@gmail.com>
2024-08-08 20:45:56 -0500

    adding which-key to profide shortcut hints

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
 			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc = "Find live [G]rep"})
 			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 			vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
