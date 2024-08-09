3b05fbe75267e72518cff49694f623d7e88f1443
Adam <aedelen42@gmail.com>
2024-08-08 22:26:28 -0500

    adding a bunch of additional key bindings from kickstart

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,11 +12,26 @@ return {
 				},
 			})
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc = "[F]ind [F]iles" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc = "[F]ind live [G]rep"})
-			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {desc = "[F]ind Old Files"})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {desc = "[F]ind [H]elp"})
-			vim.keymap.set("n", "<leader>fp", builtin.git_files, {desc = "[F]ind [P]roject"})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind live [G]rep" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+			vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "[F]ind [P]roject" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
+			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
+			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+			vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
+			vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			-- Slightly advanced example of overriding default behavior and theme
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
 
 			require("telescope").load_extension("ui-select")
 		end,
@@ -25,32 +40,32 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			-- This is your opts table
-			require("telescope").setup {
+			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown {
+						require("telescope.themes").get_dropdown({
 							-- even more opts
-						}
+						}),
 
 						-- pseudo code / specification for writing custom displays, like the one
 						-- for "codeactions"
 						-- specific_opts = {
-							--   [kind] = {
-								--     make_indexed = function(items) -> indexed_items, width,
-									--     make_displayer = function(widths) -> displayer
-										--     make_display = function(displayer) -> function(e)
-											--     make_ordinal = function(e) -> string
-												--   },
-												--   -- for example to disable the custom builtin "codeactions" display
-												--      do the following
-												--   codeactions = false,
-												-- }
-											}
-										}
-									}
-									-- To get ui-select loaded and working with telescope, you need to call
-									-- load_extension, somewhere after setup function:
-									require("telescope").load_extension("ui-select")
-								end
-							},
-						}
+						--   [kind] = {
+						--     make_indexed = function(items) -> indexed_items, width,
+						--     make_displayer = function(widths) -> displayer
+						--     make_display = function(displayer) -> function(e)
+						--     make_ordinal = function(e) -> string
+						--   },
+						--   -- for example to disable the custom builtin "codeactions" display
+						--      do the following
+						--   codeactions = false,
+						-- }
+					},
+				},
+			})
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
