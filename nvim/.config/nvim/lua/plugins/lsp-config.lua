eb075dbc2885902380a248e357acaa6d67bcd1c4
Adam <aedelen42@gmail.com>
2024-08-08 23:58:00 -0500

    adding lsp keybinding descriptions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -25,8 +25,8 @@ return {
       lspconfig.jdtls.setup({ capabilities = capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {desc = "LSP Hover"})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {desc = "[G]o to [D]efinition"})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {desc = "[G]o to [D]eclaration"})
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
       vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
