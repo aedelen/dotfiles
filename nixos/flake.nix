bc256c858e6a9bbb98257499655e159e285478d8
Adam <aedelen42@gmail.com>
2025-01-16 21:37:41 -0600

    adding nixos config

---
+++ nixos/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    nixosConfigurations = {
+    thinkpad = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos/configuration.nix
+      ];
+    };
+    };
+
+  };
+}
