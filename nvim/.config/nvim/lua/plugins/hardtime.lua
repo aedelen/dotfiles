64fc705da73f44796ff12abefc0a6b7d8fcba5d5
Adam <aedelen42@gmail.com>
2025-02-10 11:27:56 -0600

    disabling hardtime

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -1,8 +1,6 @@
 return 
 {
-   "m4xshen/hardtime.nvim",
+   --[["m4xshen/hardtime.nvim",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = {}
+   opts = {}--]]
 }
-
-
