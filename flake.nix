fa646635729d3dc7b240170e93453547053323f1
Adam <aedelen42@gmail.com>
2025-06-22 15:01:47 -0500

    renaming home.nix to thinkpad.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.adam = import ./home-manager/home.nix;
+              home-manager.users.adam = import ./home-manager/thinkpad.nix;
             }
         ];
       };
