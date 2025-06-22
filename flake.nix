cc020d3a709243574e4dec4d6e64ff8d1cab8fc5
Adam <aedelen42@gmail.com>
2025-06-22 15:22:10 -0500

    removing nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -7,19 +7,15 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixpkgs-unstable, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager,  ... }: {
 
     nixosConfigurations = {
       thinkpad = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {
-          pkgs-unstable = import nixpkgs-unstable {
-            config.allowUnfree = true;
-            system = "x86_64-linux";
-          };
         };
         modules = [
           nixos/thinkpad/configuration.nix
@@ -35,10 +31,6 @@
       dockerman = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {
-          pkgs-unstable = import nixpkgs-unstable {
-            config.allowUnfree = true;
-            system = "x86_64-linux";
-          };
         };
         modules = [
           nixos/dockerman/configuration.nix
