445c31172ad4da34cd8025bd02cc28c9aa7bc0c5
Adam <aedelen42@gmail.com>
2025-06-23 10:16:54 -0500

    trying to add telescope

--- home-manager/dockerman.nix
+++ home-manager/dockerman.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     #Put packages here
     #neovim
+    vimPlugins.telescope-nvim
   ];
   
   # Enable Neovim
@@ -28,6 +29,59 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+
+    extraLuaConfig = ''
+      -- Write lua code here --
+
+      -- Import files --
+
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      telescope-fzf-native-nvim
+      {
+        plugin = telescope-nvim;
+	config = ''
+	  require('telescope').setup({
+  	extensions = {
+      	fzf = {
+        	fuzzy = true,                    -- false will only do exact matching
+        	override_generic_sorter = true,  -- override the generic sorter
+        	override_file_sorter = true,     -- override the file sorter
+        	case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                         -- the default case_mode is "smart_case"
+      	}
+    	}
+  })
+  
+  require('telescope').load_extension('fzf')
+	'';
+	type = "lua";
+      }
+      {
+        plugin = which-key-nvim;
+	#config = ../nvim/.config/nvim/init.lua;
+	#config = '' ${builtins.readFile ../nvim/.config/nvim/lua/plugins/which-key.lua} '';
+	config = ''
+	  require("which-key").setup()
+
+		-- Document existing key chains
+		require("which-key").add({
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>f", group = "[F]ind" },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>t", group = "[T]oggle" },
+			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+		})
+
+        '';
+	type = "lua";
+      }
+    ];
+
   };
 
   # Enable home-manager and git
@@ -40,7 +94,7 @@
 
   home.file = {
     ".config/nvim/init.lua".enable = false;
-    ".config/nvim/init.lua".source = ../nvim/.config/nvim/init.lua;
+    #".config/nvim/init.lua".source = ../nvim/.config/nvim/init.lua;
     #".config/nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/adam/.dotfiles/nvim/.config/nvim/init.lua";
   };
 
