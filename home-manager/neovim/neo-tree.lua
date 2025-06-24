1e7ce7449b035b2f2cbb2221d5ec2ea2ae7d694c
Adam <aedelen42@gmail.com>
2025-06-24 11:21:19 -0500

    showing dotfiles by default in neo-tree

--- home-manager/neovim/neo-tree.lua
+++ home-manager/neovim/neo-tree.lua
@@ -1,2 +1,11 @@
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {desc = "Open file menu"})
 vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {desc = "[B]uffer [F]loat"})
+
+require("neo-tree").setup({
+	filesystem = {
+		filtered_items = {
+			hide_dotfiles = false,
+		}
+	}
+})
+
