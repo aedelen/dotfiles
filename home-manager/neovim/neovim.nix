2c5681dcff9a174a4cd7fce09a6616fb34d4222b
Adam <aedelen42@gmail.com>
2025-06-25 14:15:14 -0500

    trying to fix formatting

--- home-manager/neovim/neovim.nix
+++ home-manager/neovim/neovim.nix
@@ -1,36 +1,51 @@
-{pkgs, config, lib, ...}: { options = { neovimModule.enable = lib.mkEnableOption
-	"enables Neovim module"; }; config = lib.mkIf config.neovimModule.enable {
+{pkgs, config, lib, ...}: { 
+	options = { 
+		neovimModule.enable = lib.mkEnableOption "enables Neovim module"; 
+	}; 
+	config = lib.mkIf config.neovimModule.enable {
 		# Enable Neovim
-		programs.neovim = { enable = true; viAlias = true; vimAlias =
-			true; vimdiffAlias = true;
+		programs.neovim = { 
+			enable = true; 
+			viAlias = true; 
+			vimAlias = true; 
+			vimdiffAlias = true;
 
-			extraPackages = with pkgs; [ ripgrep fd gcc tree-sitter
+			extraPackages = with pkgs; [ 
+				ripgrep 
+				fd 
+				gcc 
+				tree-sitter
 				wl-clipboard
-
-
 			];
 
-				extraLuaConfig = '' 
+			extraLuaConfig = '' 
 				-- Write lua code here --
 
-      -- Import files -- 
-					${builtins.readFile ./settings.lua}
-      vim.lsp.enable('lua_ls') vim.lsp.enable('nil_ls') 
-      -- End of extraLuaConfig --
-				'';
+				-- Import files -- 
+				${builtins.readFile ./settings.lua}
+				vim.lsp.enable('lua_ls') 
+				vim.lsp.enable('nil_ls') 
+				-- End of extraLuaConfig --
+			'';
 
 			plugins = with pkgs.vimPlugins; [
 				telescope-fzf-native-nvim
 				telescope-ui-select-nvim tokyonight-nvim
 				nvim-lspconfig mini-icons nvim-web-devicons
 
-				{ plugin = (nvim-treesitter.withPlugins (p: [
-					p.tree-sitter-nix p.tree-sitter-vim
-					p.tree-sitter-bash p.tree-sitter-lua
-					p.tree-sitter-json ])); config =
-						''${builtins.readFile
-						./treesitter.lua}''; type
-						= "lua"; }
+				{ 
+					plugin = (nvim-treesitter.withPlugins (p: [
+						p.tree-sitter-nix 
+						p.tree-sitter-vim
+						p.tree-sitter-bash 
+						p.tree-sitter-lua
+						p.tree-sitter-json 
+					])); 
+					config = ''
+						${builtins.readFile
+						./treesitter.lua}''; 
+					type = "lua"; 
+				}
 
 				{ plugin = neo-tree-nvim; config =
 					''${builtins.readFile
