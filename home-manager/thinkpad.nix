06f729ee948328d1a5c3f37edb00625a281f04f2
Adam <aedelen42@gmail.com>
2025-06-25 14:24:41 -0500

    having thinkpad start to use neovim module

--- home-manager/thinkpad.nix
+++ home-manager/thinkpad.nix
@@ -8,6 +8,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
+    neovim/neovim.nix
   ];
 
   home = {
@@ -25,8 +26,10 @@
     obsidian
     alacritty
     filezilla
-    neovim
   ];
+
+  # Enable neovim config
+  neovimModule.enable = true;
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
