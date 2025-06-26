d5965f4126c0499c932f27daa8d3d2c384e97238
Adam <aedelen42@gmail.com>
2025-06-26 13:01:16 -0500

    removing blank line

--- home-manager/modules/gitModule.nix
+++ home-manager/modules/gitModule.nix
@@ -4,7 +4,6 @@
     gitModule.enable = lib.mkEnableOption "enables personal git config";
   };
   config = lib.mkIf config.gitModule.enable {
-
     programs.git = {
       enable = true;
       userName = "Adam Edelen";
