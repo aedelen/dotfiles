c96bbf8d896ab6617c7f1cce6a2af6fcff3ce0a3
Adam <aedelen42@gmail.com>
2025-06-26 09:14:22 -0500

    fixing telescope live_grep

--- home-manager/neovim/telescope.lua
+++ home-manager/neovim/telescope.lua
@@ -7,7 +7,7 @@ require("telescope").setup({
 	pickers = {
 		live_grep = {
 			additional_args = function(opts)
-				return {"--hidden", "!**/.git/*"}
+				return {"--hidden", "--glob=!**/.git/*"}
 			end
 		},
 	},
