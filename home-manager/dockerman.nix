e171803991a4d11ee6896841e08b84a0852ed100
Adam <aedelen42@gmail.com>
2025-06-26 08:27:35 -0500

    removing unused bindings

--- home-manager/dockerman.nix
+++ home-manager/dockerman.nix
@@ -1,6 +1,6 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{ inputs, lib, config, pkgs, ... }: {
+{ pkgs, ... }: {
 
   imports = [
     # If you want to use home-manager modules from other flakes (such as nix-colors):
