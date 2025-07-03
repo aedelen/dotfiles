3bab2a3267a7bd88bdf988b309c7593a83baaddb
Adam <aedelen42@gmail.com>
2025-07-03 13:10:04 -0500

    reverting to hyprland symlink for now due to theme changes not working

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -7,7 +7,8 @@
   config = lib.mkIf config.hyprlandConfig.enable {
     home.file."hyprlandCofigFiles" = {
       enable = true;
-      source = ../../hyprland/.config/hypr;
+      #source = ../../hyprland/.config/hypr;
+      source = config.lib.file.mkOutOfStoreSymlink "/home/adam/.dotfiles/hyprland/.config/hypr";
       target = ".config/hypr";
     };
   };
