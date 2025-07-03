b07f66e8b33a475471edd9de319c3bd169e5fcfc
Adam <aedelen42@gmail.com>
2025-07-03 13:53:43 -0500

    adding settings for thinkpad from nixos hardware repo

--- flake.nix
+++ flake.nix
@@ -8,40 +8,50 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { nixpkgs, home-manager,  ... }: {
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nixos-hardware,
+      ...
+    }:
+    {
 
-    nixosConfigurations = {
-      thinkpad = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-        };
-        modules = [
-          nixos/thinkpad/configuration.nix
-          home-manager.nixosModules.home-manager
+      nixosConfigurations = {
+        thinkpad = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+          };
+          modules = [
+            nixos/thinkpad/configuration.nix
+            nixos-hardware.nixosModules.lenovo-thinkpad-t470s
+            home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.adam = import ./home-manager/thinkpad.nix;
             }
-        ];
-      };
-
-      dockerman = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
+          ];
         };
-        modules = [
-          nixos/dockerman/configuration.nix
-          home-manager.nixosModules.home-manager {
+
+        dockerman = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+          };
+          modules = [
+            nixos/dockerman/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.adam = import ./home-manager/dockerman.nix;
-          }
-        ];
+            }
+          ];
+        };
       };
-    };
 
-  };
+    };
 }
