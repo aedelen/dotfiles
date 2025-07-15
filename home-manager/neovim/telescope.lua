451965d62bd5c8687d7c7dcc340bb9e0295148c4
Adam Edelen <aedelen42@gmail.com>
2025-07-15 18:00:24 -0500

    adding shortcut to find all files even if they aren't in git

--- home-manager/neovim/telescope.lua
+++ home-manager/neovim/telescope.lua
@@ -20,6 +20,7 @@ vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "[F]ind [P]roject"
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind live [G]rep" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc = "[F]ind [F]iles"})
 
 
 require("telescope").load_extension("ui-select")
