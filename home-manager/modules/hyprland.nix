25236ea1572485f3cdc0f96d1265a6862efae5d6
Adam Edelen <aedelen42@gmail.com>
2025-08-27 14:05:59 -0500

    removing hyprland config symlinks

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -7,12 +7,14 @@
   config = lib.mkIf config.hyprlandConfig.enable {
     home.file."waybarConfigFiles" = {
       enable = true;
-      source = config.lib.file.mkOutOfStoreSymlink "/home/adam/.dotfiles/waybar/.config/waybar";
+      source = ../../waybar/.config/waybar;
+      recursive = false;
       target = ".config/waybar";
     };
     home.file."hyprlandConfigFiles" = {
       enable = true;
-      source = config.lib.file.mkOutOfStoreSymlink "/home/adam/.dotfiles/hyprland/.config/hypr";
+      source = ../../hyprland/.config/hypr;
+      recursive = false;
       target = ".config/hypr";
     };
   };
